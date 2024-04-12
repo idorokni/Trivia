0b84e930e8d3991b99d26d95f87d587f0b126adf
Arik Vigoutov <arik.vigoutov@gmail.com>
2024-04-12 15:50:09 +0300

    changed client to send a signup message with json format

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 def main():
     import socket
+    import json
     SERVER_IP = "127.0.0.1"
     SERVER_PORT = 8326
-    HELLO_MSG_LEN = 5
 
-    #Trying to catch an error in the connecting process   
+    # Trying to catch an error in the connecting process   
     try:
         #Create a TCP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,18 +16,23 @@ def main():
     except Exception as e:
         print("Failed to connect:", e)
 
-    #Trying to catch an error in case the server suddenly disconnects    
+    # Trying to catch an error in case the server suddenly disconnects    
     try:
         
-        #Rcieving a "welcome message"
-        server_msg = sock.recv(HELLO_MSG_LEN)
+        # Sending a signup message
+        loginJson = {"username" : "user1", "password" : "1234", "mail" : "user1@gmail.com"}
+        msg_code = 51
+        
+        msg_bytes = msg_code.to_bytes(1, 'big') + (len(json.dumps(loginJson))).to_bytes(4, 'big') + json.dumps(loginJson).encode()
+        msg_bytes = bytearray(msg_bytes)
+        sock.sendall(msg_bytes)
+
+        
+        # Rcieving a response
+        server_msg = sock.recv(1024)
         server_msg = server_msg.decode()
         print(server_msg)
 
-        #Sending a response to the welcome message
-        if (server_msg == "Hello"):
-            msg = "Hello"
-            sock.sendall(msg.encode())
 
     except Exception as e:
         print("The Server Disconnected!\n")
