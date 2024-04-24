515c00d31bbdbb1c591bd9f108447506b43dfa56
Arik Vigoutov <arik.vigoutov@gmail.com>
2024-04-24 14:16:12 +0300

    #16 added regex and modified DB for address, phone and birthday

--- IDatabase.h
+++ IDatabase.h
@@ -9,7 +9,7 @@ public:
     virtual bool close() = 0;
     virtual int doesUserExist(const std::string& username) = 0;
     virtual int doesPasswordMatch(const std::string& username, const std::string& password) = 0;
-    virtual int addNewUser(const std::string& username, const std::string& password, const std::string& email) = 0;
+    virtual int addNewUser(const std::string& username, const std::string& password, const std::string& email, const std::string& address, const std::string& phone, const std::string& birthday) = 0;
 
     static IDatabase& get();
 };
--- LoginManager.h
+++ LoginManager.h
@@ -11,7 +11,7 @@ private:
 	std::vector<LoggedUser> m_loggedUsers;
 
 public:
-	bool signup(const std::string& password, const std::string& userName, const std::string& mail);
+	bool signup(const std::string& password, const std::string& userName, const std::string& mail, const std::string& address, const std::string& phone, const std::string& birthday);
 	bool login(const std::string& password, const std::string& userName);
 	bool logout(const std::string& userName);
 
