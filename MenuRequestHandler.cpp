138ac63bbc69225a992c4681adfc6b0dcc60eb5b
ido rokni <ido.rokni@gmail.com>
2024-05-18 22:33:31 +0300

    #24 added all private functions to header

--- MenuRequestHandler.cpp
+++ MenuRequestHandler.cpp
@@ -1,10 +1,10 @@
 #include "MenuRequestHandler.h"
 
+bool MenuRequestHandler::isRequestRelevant(const RequestInfo& info) {
+	return info.id == (int)RequestCode::LOGOUT_REQUEST_CODE || info.id == (int)RequestCode::CREATE_ROOM_REQUEST_CODE || info.id == (int)RequestCode::GET_PLAYERS_IN_ROOM_REQUEST_CODE || info.id == (int)RequestCode::GET_ROOM_REQUEST_CODE || info.id == (int)RequestCode::JOIN_ROOM_REQUEST_CODE || info.id == (int)RequestCode::GET_USER_STATISTICS_REQUEST_CODE;
+}
+
 RequestResult MenuRequestHandler::handleRequest(const RequestInfo& info) {
 	RequestResult reasult;
 	return reasult;
-}
-
-bool MenuRequestHandler::isRequestRelevant(const RequestInfo& info) {
-	return true;
 }
--- MenuRequestHandler.h
+++ MenuRequestHandler.h
@@ -7,4 +7,12 @@ class MenuRequestHandler : public IRequestHandler {
 public:
 	bool isRequestRelevant(const RequestInfo& info) override;
 	RequestResult handleRequest(const RequestInfo& info) override;
+private:
+	RequestResult signout(const RequestInfo& info);
+	RequestResult getRooms(const RequestInfo& info);
+	RequestResult getPlayersInRoom(const RequestInfo& info);
+	RequestResult getPersonalStats(const RequestInfo& info);
+	RequestResult getHighScore(const RequestInfo& info);
+	RequestResult joinRoom(const RequestInfo& info);
+	RequestResult createRoom(const RequestInfo& info);
 };
