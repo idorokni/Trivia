d968dbd00072b2bb74fd7d3355742a0fa797f5cb
ido rokni <ido.rokni@gmail.com>
2024-06-03 22:24:48 +0300

    #27 refactored user of login and sign up for lambda

--- Client/Client/MVVM/ViewModel/CreateRoomViewModel.cs
+++ Client/Client/MVVM/ViewModel/CreateRoomViewModel.cs
@@ -9,6 +9,41 @@ namespace Client.MVVM.ViewModel
 {
     internal class CreateRoomViewModel : ObservableObject
     {
+        private string _name;
+        private string _amountOfUsers;
+        private string _amountOfQuestions;
+        private string _timePerQuestions;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string AmountOfUsers
+        {
+            get { return _amountOfUsers; }
+            set { _amountOfUsers = value; }
+        }
+
+        public string AmountOfQuestions
+        {
+            get { return _amountOfQuestions; }
+            set { _amountOfQuestions = value; }
+        }
+
+        public string TimePerQuestions
+        {
+            get { return _timePerQuestions; }
+            set { _timePerQuestions = value; }
+        }
+
+        public RelayCommand SubmitCreateRoomCommand { get; set; }
+
+        public CreateRoomViewModel()
+        {
+        }
+
 
     }
 }
