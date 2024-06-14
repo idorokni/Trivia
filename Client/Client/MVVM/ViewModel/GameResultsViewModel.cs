acc490d61bf45cff0dc83ec774793003c5b0ea81
ido rokni <ido.rokni@gmail.com>
2024-06-15 01:42:33 +0300

    #38 added listviews for end screen

---
+++ Client/Client/MVVM/ViewModel/GameResultsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Client.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.MVVM.ViewModel
+{
+    internal class GameResultsViewModel : ObservableObject
+    {
+        private ObservableCollection<string> _userNames;
+        private ObservableCollection<uint> _correctAnswers;
+        private ObservableCollection<uint> _wrongAnswers;
+        private ObservableCollection<uint> _averageAnswerTime;
+
+
+    }
+}
